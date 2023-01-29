195aa801708f6bcd56bf5df676f5d012525e4c91
Or-Reff <100279705+Or-Reff@users.noreply.github.com>
2023-01-29 21:14:48 +0200

    frontend base implementation & box stracture

--- backend/app.js
+++ backend/app.js
@@ -1,59 +1,77 @@
 // imports
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const stateModel = require('./models/stateModel');
-const { default: mongoose } = require('mongoose');
+const stateModel = require("./models/stateModel");
+const { default: mongoose } = require("mongoose");
 
 const app = express();
 
-mongoose.set('strictQuery', false)
-mongoose.connect("mongodb+srv://orMongoAdmin:sGkkSwZgVXMMe3nq@cluster0.aftzbrv.mongodb.net/DigitalTeamTest")
-    .then(() => {
-        console.log('Connected to database!');
-    })
-    .catch(() => {
-        console.log('Connection failed!');
-    })
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(
+    "mongodb+srv://orMongoAdmin:sGkkSwZgVXMMe3nq@cluster0.aftzbrv.mongodb.net/DigitalTeamTest"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "Origin,X-Requested-With, Content-Type, Accept");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
 });
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post("/api/posts", (req, res, next) => {
-    const stateModel = new StateModel({
-        title: req.body.title,
-        content: req.body.content
-    })
-    stateModel.save().then(createdPost => {
-        res.status(201).json({
-            message: 'Post added successfuly!',
-            postId: createdPost._id
-        });
-    })
-})
-
-app.get("/api/state", (req, res, next) => {
-  var id = req.query.id; // $_GET["id"]
-  stateModel.find().then(documents => {
-        res.status(200).json({
-            message: "Posts fetched succesfully!",
-            posts: documents
-        });
-    })
+  const stateModel = new StateModel({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  stateModel.save().then((createdPost) => {
+    res.status(201).json({
+      message: "Post added successfuly!",
+      postId: createdPost._id,
+    });
+  });
 });
 
+app.get("/api/state", async (req, res, next) => {
+  const id = req.query.id; // $_GET["id"]
+
+  const state = await stateModel.updateOne(
+    { id },
+    { state: "something something" },
+    {
+      upsert: true,
+      multi: false,
+    }
+  );
+
+  // stateModel.find(id).then(documents => {
+  //       res.status(200).json({
+  //           message: "Posts fetched succesfully!",
+  //           posts: documents
+  //       });
+  //   })
+});
 
 app.delete("/api/posts/:id", (req, res, next) => {
-  stateModel.deleteOne({ _id: req.params.id }).then(result => {
-        console.log(result);
-    })
-    res.status(200).json({ message: "Post Deleted!" })
+  stateModel.deleteOne({ _id: req.params.id }).then((result) => {
+    console.log(result);
+  });
+  res.status(200).json({ message: "Post Deleted!" });
 });
 
 module.exports = app;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BoxModule } from "./modules/box/box.module";
+import { BoxServiceComponent } from './services/box-service/box-service.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        BoxServiceComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        BoxModule
+    ]
 })
 export class AppModule { }
