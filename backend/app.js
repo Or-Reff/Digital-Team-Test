1cb3e4295d4a9c074b5417e6b1d411a30e69268d
Or-Reff <100279705+Or-Reff@users.noreply.github.com>
2023-01-31 18:45:48 +0200

    routes backend done

--- backend/app.js
+++ backend/app.js
@@ -5,10 +5,12 @@ const cors = require("cors");
 const enumState = require("./enums/state.ts");
 const cron = require("node-cron");
 
+
 const stateModel = require("./models/stateModel");
 const { default: mongoose } = require("mongoose");
 
 const app = express();
+const statesRoute = require("./routes/states");
 
 const enumStateHard = ["KWS_KERIDOS", "KWS_KERIDOS_YG", "UNKNOWN", "ERROR"];
 
@@ -33,43 +35,7 @@ app.use(
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-/**initializeData to mongoDB */
-app.get("/api/initializeData", async (req, res) => {
-  try {
-    const stateData = [];
-    if ((await stateModel.countDocuments({})) === 0) {
-      for (let i = 0; i < 45; i++) {
-        const newState =
-          enumStateHard[Math.floor(Math.random() * enumStateHard.length)];
-        stateData.push({ index: i, state: newState });
-      }
-      stateModel.insertMany(stateData, (error, docs) => {
-        if (error) {
-          res.status(400).json({ message: "Cannot fetch data" });
-        } else {
-          res.status(200).json({ message: `Inserted ${docs.length} documents` });
-        }
-      });
-    } else {
-      res.status(200).json({ message: `Already initialized data` });
-    }
-  } catch (ERROR) {}
-});
-/**FetchData to the frontend UI */
-app.get("/api/fetchData", async (req, res) => {
-  try {
-    const shouldFetchAll = req.query.shouldFetchAll; // $_GET["shouldFetchAll"]
-    console.log(shouldFetchAll);
 
-    const filter = {};
-    if (!shouldFetchAll) {
-      filter.isUpdated = true;
-    }
-    res.status(200).send(await stateModel.find(filter).sort({ id: 1 }));
-  } catch (ERROR) {
-    res.sendStatus(400).json({ message: "Cannot fetch data" });
-  }
-});
 /**Cron schedule every 0.5 seconds to update the Database*/
 cron.schedule("*/" + "0,5 * * * * *", async function () {
   console.log("Inside cron schedule");
@@ -103,26 +69,6 @@ cron.schedule("*/" + "0,5 * * * * *", async function () {
   // await bulk.execute();
 });
 
-app.route("/api/update-state").put(async (req, res) => {
-  console.log("inside route");
-  const enumStateHard = ["KWS_KERIDOS", "KWS_KERIDOS_YG", "UNKNOWN", "ERROR"];
-  const docs = await stateModel.find();
+app.use("/api", statesRoute);
 
-  const promises = docs.map(async (doc) => {
-    const randStateValue =
-      enumStateHard[Math.floor(Math.random() * enumStateHard.length)];
-    return await stateModel.updateOne(
-      { _id: doc._id },
-      { state: randStateValue },
-      {
-        upsert: true,
-        multi: false,
-      }
-    );
-  });
-
-  const result = await Promise.all(promises);
-
-  res.status(200).json({ result });
-});
 module.exports = app;
---
+++ backend/routes/states.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const stateModel = require("../models/stateModel");
+
+// const statesController = require("../controllers/states");
+
+const router = express.Router();
+
+/**initializeData to mongoDB */
+router.get("/initializeData", async (req, res) => {
+  try {
+    const stateData = [];
+    if ((await stateModel.countDocuments({})) === 0) {
+      for (let i = 0; i < 45; i++) {
+        const newState =
+          enumStateHard[Math.floor(Math.random() * enumStateHard.length)];
+        stateData.push({ index: i, state: newState });
+      }
+      stateModel.insertMany(stateData, (error, docs) => {
+        if (error) {
+          console.log("3");
+          return res.status(400).json({ message: "Cannot fetch data" });
+        } else {
+          console.log("4");
+
+          return res
+            .status(200)
+            .json({ message: `Inserted ${docs.length} documents` });
+        }
+      });
+    } else {
+      console.log("5");
+
+      return res.status(200).json({ message: `Already initialized data` });
+    }
+  } catch (ERROR) {}
+});
+
+/**FetchData to the frontend UI */
+router.get("/fetchData", async (req, res) => {
+  try {
+    const shouldFetchAll = req.query.shouldFetchAll; // $_GET["shouldFetchAll"]
+    console.log(shouldFetchAll);
+
+    const filter = {};
+    if (!shouldFetchAll) {
+      filter.isUpdated = true;
+    }
+    console.log("1");
+
+    return res.status(200).send(await stateModel.find(filter).sort({ id: 1 }));
+  } catch (error) {
+    console.log("2");
+
+    return res.sendStatus(400).json({ message: "Cannot fetch data" });
+  }
+});
+
+module.exports = router;
